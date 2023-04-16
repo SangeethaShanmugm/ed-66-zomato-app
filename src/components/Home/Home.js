6cd7ef1c7c521a524852b3cb8a61f0bdae03fdb3
SangeethaShanmugm <MY_NAME@example.com>
2023-04-16 19:51:30 +0530

    updated

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import Header from "../../Header";
+import QuickSearch from "./QuickSearch";
+import Search from "./Search";
+
+export const Home = () => {
+  return (
+    <>
+      <Header />
+      <Search />
+      <QuickSearch />
+    </>
+  );
+};
---
+++ src/components/Home/Search.js
@@ -0,0 +1,80 @@
+import React, { Component } from "react";
+import "./Search.css";
+
+const lurl = "http://localhost:5000/locations";
+const rurl = "http://localhost:5000/restaurants?stateId=";
+
+export default class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      location: "",
+      restaurants: "",
+    };
+  }
+  renderCity = (data) => {
+    if (data) {
+      return data.map((item) => {
+        return (
+          <option key={item._id} value={item.state_id}>
+            {item.state}
+          </option>
+        );
+      });
+    }
+  };
+
+  renderRest = (data) => {
+    if (data) {
+      return data.map((item) => {
+        return (
+          <option key={item._id} value={item.restaurant_id}>
+            {item.restaurant_name}
+          </option>
+        );
+      });
+    }
+  };
+  handleCity = (event) => {
+    const stateId = event.target.value;
+    fetch(`${rurl}${stateId}`, { method: "GET" })
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({ restaurants: data });
+        console.log(data);
+      });
+  };
+  render() {
+    return (
+      <div id="search">
+        <div id="logo">
+          <span>e!</span>
+        </div>
+        <div id="heading">
+          <p>Find the best restaurants, cafés, and bars</p>
+        </div>
+        <div id="dropdown">
+          <select onChange={this.handleCity}>
+            <option>Please type a location</option>
+            <option>----SELECT CITY----</option>
+            {this.renderCity(this.state.location)}
+          </select>
+          <select id="select-style">
+            <option>----SELECT RESTAURANTS----</option>
+            {this.renderRest(this.state.restaurants)}
+          </select>
+        </div>
+      </div>
+    );
+  }
+
+  //api calling on page load
+  componentDidMount() {
+    fetch(lurl, { method: "GET" })
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({ location: data });
+        console.log(data);
+      });
+  }
+}
